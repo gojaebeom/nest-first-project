ead62d5ba3a233e8c1f7bf36716e8cf73d6a6cc9
gojaebeom <const.gjb@gmail.com>
2022-03-15 09:56:22 +0900

    update

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,15 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Cats } from './cats.entity';
 import { CatRequestDto } from './dto/cats.request.dto';
 import * as bcrypt from 'bcrypt';
 import { CatResponseDto } from './dto/cats.response.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CatsService {
   constructor(
-    @InjectRepository(Cats) private catsRepository: Repository<Cats>,
+    @InjectRepository(Cats) private readonly catsRepository: Repository<Cats>,
   ) {}
 
   async signUp(body: CatRequestDto): Promise<CatResponseDto> {
@@ -21,9 +21,9 @@ export class CatsService {
 
     const catData = await this.catsRepository.findOne({ where: { email } });
     if (catData) {
-      throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.');
-      // throw new HttpException('해당하는 고양이는 이미 존재합니다.', 403);
+      throw new UnauthorizedException('이미 가입된 고양이입니다!');
     }
+
     const saltOrRounds = 10;
     const hashPassworld = await bcrypt.hash(password, saltOrRounds);
     cat.password = hashPassworld;
